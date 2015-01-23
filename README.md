198f7af21940d57094f707262ea073cd814973e6
Robert <robert@atroo.de>
2015-01-23 14:07:34 +0100

    added readme and dependency contexts

---
+++ README.md
@@ -0,0 +1,29 @@
+# i18next-resource-store-loader
+
+this loader generates the resStore config needed for i18next to avoid loading language ressource via http requests. It generates this config from a directory.
+
+## Usage
+
+Assuming the following Filestructure
+``` javascript
+-app
+ -src
+  -assets
+   -i18n
+    index.js
+    -de
+     translation.json
+    -en
+     translation.json
+```
+Use the loader in the following way:
+``` javascript
+var i18n = require("i18next-client");
+var resBundle = require("i18next-resource-store-loader!../assets/i18n/index.js");
+
+//i18n
+i18n.init({
+    resStore: resBundle
+});
+```
+And your done. The index.js can be empty, its just needed to point the loader to the locales root directory.
--- index.js
+++ index.js
@@ -23,13 +23,15 @@ module.exports = function (indexContent) {
         files = fs.readdirSync(path.join(baseDirectory, dirname)).filter(function (file) {
             return fs.statSync(path.join(baseDirectory, dirname, file)).isFile();
         });
-        var filename, extname, basename, content;
+        var filename, extname, basename, content, pathstring;
         for (var j = 0, len2 = files.length; j < len2; j++) {
             filename = files[j];
-            extname = path.extname(path.join(baseDirectory, dirname, filename));
-            basename = path.basename(path.join(baseDirectory, dirname, filename), extname);
-            content = fs.readFileSync(path.join(baseDirectory, dirname, filename));
+            pathstring = path.join(baseDirectory, dirname, filename);
+            extname = path.extname(pathstring);
+            basename = path.basename(pathstring, extname);
+            content = fs.readFileSync(pathstring);
             resBundle[dirname][basename] = JSON.parse(content);
+            this.addDependency(pathstring);
         }
     }
     return "module.exports = " + JSON.stringify(resBundle);

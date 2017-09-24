f9093aa5e146841e07a576eca630267ee5d85d8e
Kevin Ross <kevin.ross@alienfast.com>
2017-09-23 20:45:45 -0500

    introduce yaml parsing

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const path = require("path");
-const fs = require("fs");
-const merge = require("lodash/merge");
-const globAll = require('glob-all');
-const loaderUtils = require('loader-utils');
+const path = require("path")
+const fs = require("fs")
+const merge = require("lodash/merge")
+const globAll = require('glob-all')
+const loaderUtils = require('loader-utils')
+const yaml = require("js-yaml")
 
 function enumerateLangDirs (dir) {
   return fs.readdirSync(dir).filter(function (file) {
-    return fs.statSync(path.join(dir, file)).isDirectory();
-  });
+    return fs.statSync(path.join(dir, file)).isDirectory()
+  })
 }
 
 //https://github.com/jpillora/node-glob-all#usage
@@ -17,37 +18,40 @@ function findAll (globs, cwd) {
 }
 
 module.exports = function (indexContent) {
-  this.cacheable && this.cacheable();
-  const options = loaderUtils.getOptions(this) || {};
+  this.cacheable && this.cacheable()
+  const options = loaderUtils.getOptions(this) || {}
 
   if (!options.include) {
     options.include = [ "**/*.json", "**/*.yml", "**/*.yaml" ]
   }
 
-  let baseDirectory = path.dirname(this.resource); // this is the absolute path to the index.js in the top level locales dir
+  let baseDirectory = path.dirname(this.resource) // this is the absolute path to the index.js in the top level locales dir
 
-  //alls sub directories match to languages
-  let resBundle = {}, lang, files, basefiles, overridefiles;
-  //const langDirs = inOverrideMode ? subdirsbase : subdirs;
+  // all subdirectories match language codes
+  let resBundle = {}
   const langDirs = enumerateLangDirs(baseDirectory)
-  for (let i = 0, len = langDirs.length; i < len; i++) {
+  for (let i = 0; i < langDirs.length; i++) {
     //all files within the sub directory map to namespaces
-    lang = langDirs[ i ];
-    resBundle[ lang ] = {};
+    const lang = langDirs[ i ]
+    resBundle[ lang ] = {}
 
     const fullLangPath = path.join(baseDirectory, lang)
-    const filesToAdd = findAll(options.include, fullLangPath);
-    for (let j = 0, len2 = filesToAdd.length; j < len2; j++) {
-      const fullPath = filesToAdd[ j ];
-      const extname = path.extname(fullPath);
-      const basename = path.basename(fullPath, extname);
-      const content = fs.readFileSync(fullPath);
-      const fileData = JSON.parse(content);
-      resBundle[ lang ] = fileData;
-      this.addDependency(fullPath);
+    const filesToAdd = findAll(options.include, fullLangPath)
+    for (let j = 0; j < filesToAdd.length; j++) {
+      const fullPath = filesToAdd[ j ]
+      const content = fs.readFileSync(fullPath)
+      const extname = path.extname(fullPath)
+      let fileData
+      if (extname === '.yaml' || extname === '.yml') {
+        fileData = yaml.safeLoad(content)
+      } else {
+        fileData = JSON.parse(content)
+      }
+      resBundle[ lang ] = fileData
+      this.addDependency(fullPath)
     }
 
-    this.addContextDependency(fullLangPath);
+    this.addContextDependency(fullLangPath)
   }
-  return "module.exports = " + JSON.stringify(resBundle);
+  return "module.exports = " + JSON.stringify(resBundle)
 }
--- test/suites/basicTest.js
+++ test/suites/basicTest.js
@@ -1,44 +1,36 @@
-var fs = require('fs');
-var path = require('path');
-
-var chai = require('chai');
-var expect = chai.expect;
-
-var loader = require('../../index');
+const fs = require('fs');
+const path = require('path');
+const chai = require('chai');
+const expect = chai.expect;
+const loader = require('../../index');
 
 describe("basic", function () {
-    // <SETUP> ///////////////////////////////////////
-    beforeEach(function (done) {
+  [ 'yaml', 'json' ].forEach((type) => {
+    context(type, () => {
+      beforeEach(function (done) {
         //mock webpack loader this scope
-        var emptFn = function () {};
+        const emptFn = function () {};
         thisScope = {
-            addDependency: emptFn,
-            addContextDependency: emptFn,
-            cacheable: emptFn,
-            resource: path.join(__dirname, "../data/i18n/index.js")
+          addDependency: emptFn,
+          addContextDependency: emptFn,
+          cacheable: emptFn,
+          resource: path.join(__dirname, `../data/basic-${type}/index.js`)
         }
         done();
-    });
+      });
 
-    afterEach(function (done) {
-        done();
-    });
-    // </SETUP> ///////////////////////////////////////
-
-
-    // <TESTS> ///////////////////////////////////////
-    it("should generate a resStore structure", function () {
-        var res = loader.call(thisScope, "index.js");
-        var resStore = eval(res);
+      it("should generate a resStore structure", function () {
+        const res = loader.call(thisScope, "index.js");
+        const resStore = eval(res);
         expect(resStore.dev).to.be.an('object');
         expect(resStore.de).to.be.an('object');
         expect(resStore.en).to.be.an('object');
         expect(resStore.fr).to.be.an('object');
-    });
+      });
 
-    it("should have the correct sub structures", function () {
-        var res = loader.call(thisScope, "index.js");
-        var resStore = eval(res);
+      it("should have the correct sub structures", function () {
+        const res = loader.call(thisScope, "index.js");
+        const resStore = eval(res);
 
         expect(resStore.dev.main.test).to.be.a('string');
         expect(resStore.dev.main.test).to.be.equal('Dev dev dev!');
@@ -51,25 +43,26 @@ describe("basic", function () {
 
         expect(resStore.fr.main.test).to.be.a('string');
         expect(resStore.fr.main.test).to.be.equal('Ceci est un test!');
-    });
+      });
 
-    it("should process include", function () {
+      it("should process include", function () {
         //thisScope.query = '?include=\\.json$';
         thisScope.query = '?{include: ["**/*.json"]}';
-        thisScope.addDependency = function(path) {
-            expect(path).to.not.contain('main.nonjson');
+        thisScope.addDependency = function (path) {
+          expect(path).to.not.contain('main.nonjson');
         };
 
-        var res = loader.call(thisScope, "index.js");
-    });
+        const res = loader.call(thisScope, "index.js");
+      });
 
-    it("should not process files that are excluded", function () {
-        //thisScope.query = '?exclude=\\.nonjson$';
-        thisScope.query = '?{include: ["**/*.json", "!**/exclude.json"]}';
-        thisScope.addDependency = function(path) {
-            expect(path).to.not.contain('exclude.json');
+      it("should not process files that are excluded", function () {
+        thisScope.query = `?{include: ["**/*.${type}", "!**/exclude.${type}"]}`;
+        thisScope.addDependency = function (path) {
+          expect(path).to.not.contain('exclude.json');
         };
 
-        var res = loader.call(thisScope, "index.js");
-    });
-});
+        const res = loader.call(thisScope, "index.js");
+      });
+    })
+  });
+})
